165c87313a8abe8eb618dbb4f239d46f4ed8b0e4
Rahul-8283 <lsrahul12@gmail.com>
2026-05-24 16:17:06 +0530

    refactor: complete axios config logic.

--- Frontend/src/lib/api.ts
+++ Frontend/src/lib/api.ts
@@ -1,9 +1,10 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const BASE = import.meta.env.VITE_BACKEND_URL_PROD || import.meta.env.VITE_BACKEND_URL_DEV || ''
+const MODE = import.meta.env.VITE_MODE;
+const BASE_URL = MODE === 'production' ? import.meta.env.VITE_BACKEND_URL_PROD : import.meta.env.VITE_BACKEND_URL_DEV;
 
 const api = axios.create({
-  baseURL: BASE,
+  baseURL: BASE_URL || 'http://localhost:8000',
   headers: {
     'Content-Type': 'application/json',
   },
